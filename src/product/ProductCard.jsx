1400f146ac4e5b63dfd7d6557db0b5188b8ebe9f
vansh167 <vanshdoc7gmail.com>
2025-11-27 18:05:14 +0530

    02

--- src/product/ProductCard.jsx
+++ src/product/ProductCard.jsx
@@ -1,15 +1,33 @@
+import { Link } from "react-router-dom";
+
 export default function ProductCard({ product }) {
     return (
-        <div className="border border-gray-200 rounded-lg overflow-hidden shadow-sm hover:shadow-lg transition">
-            <img src={product.image} alt={product.name} className="w-full h-56 object-cover" />
-            <div className="p-4">
-                <h3 className="text-gray-800 font-medium text-sm mb-1">{product.name}</h3>
-                <p className="text-gray-500 text-xs mb-2">{product.category}</p>
-                <div className="flex justify-between items-center">
-                    <span className="text-[#2BB673] font-semibold">₹{product.price}</span>
-                    <button className="text-gray-400 hover:text-[#2BB673]">♥</button>
+        <Link to={`/product/${product.id}`}>
+            <div className="border border-gray-200 rounded-lg overflow-hidden shadow-sm hover:shadow-lg transition cursor-pointer">
+                <img
+                    src={product.image}
+                    alt={product.name}
+                    className="w-full h-56 object-cover"
+                />
+
+                <div className="p-4">
+                    <h3 className="text-gray-800 font-medium text-sm mb-1">
+                        {product.name}
+                    </h3>
+
+                    <p className="text-gray-500 text-xs mb-2">
+                        {product.category}
+                    </p>
+
+                    <div className="flex justify-between items-center">
+                        <span className="text-[#2BB673] font-semibold">₹{product.price}</span>
+
+                        <button className="text-gray-400 hover:text-[#2BB673]">
+                            ♥
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
